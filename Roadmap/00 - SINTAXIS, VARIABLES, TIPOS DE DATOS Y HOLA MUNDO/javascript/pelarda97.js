2b51b831856e1421cd8153cacb64a76233b9f2ed
pelarda97 <manelpelarda@gmail.com>
2024-11-16 16:12:04 +0100

    pelarda97.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/pelarda97.js
@@ -0,0 +1,14 @@
+//https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//Comentario en una línea
+
+/*Comentario
+en varias
+líneas*/
+
+let Kratos = "God of War";
+const Atreus = "Loki";
+let enteros =2;
+let booleano = true;
+
+console.log(¡Hola, JavaScript!);
